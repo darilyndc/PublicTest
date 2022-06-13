8d2387e24ba4b70232e3f44d5516a9c67023d954
darilyndc <107413996+darilyndc@users.noreply.github.com>
2022-06-13 22:32:44 +0800

    UPDATED DARS 10.30
    SDASDSAD

--- PublicTestEclipse/src/main/java/PublicTestEclipse/PublicTestEclipse/App.java
+++ PublicTestEclipse/src/main/java/PublicTestEclipse/PublicTestEclipse/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hesadallos Wodfdsfrlasdadasdd!" );    
+        System.out.println( "HELLO DARS!" );    
     }
 }
