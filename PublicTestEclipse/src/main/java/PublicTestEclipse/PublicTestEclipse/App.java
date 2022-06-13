0f18f7397d75f4606a15627a5ca6e711f3659270
darilyndc <107413996+darilyndc@users.noreply.github.com>
2022-06-13 22:30:49 +0800

    Update App.java

--- PublicTestEclipse/src/main/java/PublicTestEclipse/PublicTestEclipse/App.java
+++ PublicTestEclipse/src/main/java/PublicTestEclipse/PublicTestEclipse/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hesadallos Wodfdsfrld!" );    
+        System.out.println( "Hesadallos Wodfdsfrlasdadasdd!" );    
     }
 }
